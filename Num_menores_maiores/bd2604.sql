f1565eca8db42b2ee3ee64309f28af34175ffd8c
Sheila  Santos <88636828+SheilaSantosPy@users.noreply.github.com>
2022-12-19 22:09:07 -0300

    Add files via upload

---
+++ Num_menores_maiores/bd2604.sql
@@ -0,0 +1,17 @@
+create table products (
+    id number(3) primary key,
+    name varchar2(60),
+    amount number(7),
+    price number(10,2));
+    
+insert into products values(11,'Two-door wardrobe',100,80);
+insert into products values(22,'Dining table',1000,560);
+insert into products values(33,'Towel holder',10000,5.50);
+insert into products values(44,'Computer desk',350,100);
+insert into products values(55,'Chair',3000,210.64);
+insert into products values(66,'Single bed',750,99);
+
+
+-- O setor financeiro da empresa precisa de um relatório que mostre o código e o nome dos produtos cujo preço são menores que 10 ou maiores que 100.
+select * from products
+where price < 10 or price > 100;
